bb26fefc56dafcf70096786228d205f09a2c6998
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-10-26 17:06:41 +0530

    initial commit

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -24,24 +24,52 @@ function logInToPaas() {
 	chmod +x "${KUBECTL_BIN}"
 	echo "Removing current Kubernetes configuration"
 	rm -rf "${KUBE_CONFIG_PATH}" || echo "Failed to remove Kube config. Continuing with the script"
+        downloadGCloud
 	echo "Logging in to Kubernetes API [${apiUrl}], with cluster name [${k8sClusterName}] and user [${k8sClusterUser}]"
-	"${KUBECTL_BIN}" config set-cluster "${k8sClusterName}" --server="https://${apiUrl}" --certificate-authority="${k8sCa}" --embed-certs=true
+	#"${KUBECTL_BIN}" config set-cluster "${k8sClusterName}" --server="https://${apiUrl}" --certificate-authority="${k8sCa}" --embed-certs=true
 	# TOKEN will get injected as a credential if present
-	if [[ "${TOKEN}" != "" ]]; then
-		"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --token="${TOKEN}"
-	elif [[ "${k8sTokenPath}" != "" ]]; then
-		local tokenContent
-		tokenContent="$(cat "${k8sTokenPath}")"
-		"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --token="${tokenContent}" --kubeconfig="${KUBE_CONFIG_PATH}"
-	else
-		"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --certificate-authority="${k8sCa}" --client-key="${k8sClientKey}" --client-certificate="${k8sClientCert}"  --kubeconfig="${KUBE_CONFIG_PATH}"
-	fi
-	"${KUBECTL_BIN}" config set-context "${k8sSystemName}" --cluster="${k8sClusterName}" --user="${k8sClusterUser}"  --kubeconfig="${KUBE_CONFIG_PATH}"
-	"${KUBECTL_BIN}" config use-context "${k8sSystemName}" --kubeconfig="${KUBE_CONFIG_PATH}"
-
+	#if [[ "${TOKEN}" != "" ]]; then
+	#	"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --token="${TOKEN}"
+	#elif [[ "${k8sTokenPath}" != "" ]]; then
+	#	local tokenContent
+	#	tokenContent="$(cat "${k8sTokenPath}")"
+	#	"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --token="${tokenContent}" --kubeconfig="${KUBE_CONFIG_PATH}"
+	#else
+	#	"${KUBECTL_BIN}" config set-credentials "${k8sClusterUser}" --certificate-authority="${k8sCa}" --client-key="${k8sClientKey}" --client-certificate="${k8sClientCert}"  --kubeconfig="${KUBE_CONFIG_PATH}"
+	#fi
+	#"${KUBECTL_BIN}" config set-context "${k8sSystemName}" --cluster="${k8sClusterName}" --user="${k8sClusterUser}"  --kubeconfig="${KUBE_CONFIG_PATH}"
+	#"${KUBECTL_BIN}" config use-context "${k8sSystemName}" --kubeconfig="${KUBE_CONFIG_PATH}"
+	#./google-cloud-sdk/bin/gcloud init
+	gcloud container clusters get-credentials test --zone us-central1-c --project third-apex-181907
 	echo "CLI version"
 	"${KUBECTL_BIN}" version
 }
+
+function downloadGCloud() {
+ if [[ "${OSTYPE}" == linux* ]]; then
+			OS_TYPE="linux"
+		else
+			OS_TYPE="darwin"
+		fi
+		GCLOUD_VERSION="${GCLOUD_VERSION:-172.0.1}"
+		GCLOUD_ARCHIVE="${GCLOUD_ARCHIVE:-google-cloud-sdk-${GCLOUD_VERSION}-${OS_TYPE}-x86_64.tar.gz}"
+		GCLOUD_PARENT_PATH="${GCLOUD_PARENT_PATH:-${HOME}/gcloud}"
+		GCLOUD_PATH="${GCLOUD_PATH:-${GCLOUD_PARENT_PATH}/google-cloud-sdk}"
+		if [[ -x "${GCLOUD_PATH}" ]]; then
+			echo "gcloud already downloaded - skipping..."
+			exit 0
+		fi
+		wget -P "${GCLOUD_PARENT_PATH}/" \
+                "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${GCLOUD_ARCHIVE}"
+		pushd "${GCLOUD_PARENT_PATH}/" || exit
+		tar xvf "${GCLOUD_ARCHIVE}"
+		rm -vf -- "${GCLOUD_ARCHIVE}"
+		echo "Running the installer"
+		"${GCLOUD_PATH}/install.sh"
+		popd || exit
+
+}
+
 
 function testDeploy() {
 	local appName
