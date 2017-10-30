6d82622415bc02316e6078fbf957eee3aaff4bee
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-10-30 19:55:36 +0530

    test

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -138,8 +138,8 @@ function deploy_project {
 
 	echo "Deploying ${project_name} to artifactory"
 	PROJECT_PARENT_PATH="${HOME}/${project_name}"
-	#mkdir "${PROJECT_PARENT_PATH}"
-	pushd "${PROJECT_PARENT_PATH}"
+	mkdir "${project_name}"
+	pushd ""${project_name}"
 	rm -rf "${project_name}"
 	git clone "${project_repo}" && cd "${project_name}"
 	chmod a+x mvnw
