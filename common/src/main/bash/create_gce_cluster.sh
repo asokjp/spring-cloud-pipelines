846b0ac05eac2ee81b9eec6e1a98d97db80db2e3
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-12-14 17:52:03 +0530

    commit

--- common/src/main/bash/create_gce_cluster.sh
+++ common/src/main/bash/create_gce_cluster.sh
@@ -31,7 +31,10 @@ gcloud container clusters create test --zone us-central1-a --num-nodes 1 --machi
 
 gcloud container clusters get-credentials test --zone us-central1-a --project dulcet-hulling-185607
 
-setup-namespaces
+mkdir -p build
+		createNamespace "sc-pipelines-test"
+		createNamespace "sc-pipelines-stage"
+		createNamespace "sc-pipelines-prod"
 
 function downloadGCloud() {
  if [[ "${OSTYPE}" == linux* ]]; then
