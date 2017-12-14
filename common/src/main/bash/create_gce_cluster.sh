82528dcb34c265a30fb237fdaa841a38bca9f826
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-12-14 11:57:48 +0530

    commit

--- common/src/main/bash/create_gce_cluster.sh
+++ common/src/main/bash/create_gce_cluster.sh
@@ -15,11 +15,41 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
     chmod +x ./kubectl && \
     mv ./kubectl /usr/local/bin/kubectl
 	
+GCLOUD_PARENT_PATH="${GCLOUD_PARENT_PATH:-${HOME}/gcloud}"
+		GCLOUD_PATH="${GCLOUD_PATH:-${GCLOUD_PARENT_PATH}/google-cloud-sdk}"
+		if ! [ -x "${GCLOUD_PATH}" ]; then
+			echo "installing gcloud.."
+			downloadGCloud
+			gcloud init
+		fi
 
+	
 gcloud container clusters create test --zone us-central1-a --num-nodes 1 --machine-type n1-standard-4
 
 gcloud container clusters get-credentials test --zone us-central1-a --project dulcet-hulling-185607
 
 echo kubectl get nodes
+
+
+function downloadGCloud() {
+ if [[ "${OSTYPE}" == linux* ]]; then
+			OS_TYPE="linux"
+		else
+			OS_TYPE="darwin"
+		fi
+		GCLOUD_VERSION="${GCLOUD_VERSION:-172.0.1}"
+		GCLOUD_ARCHIVE="${GCLOUD_ARCHIVE:-google-cloud-sdk-${GCLOUD_VERSION}-${OS_TYPE}-x86_64.tar.gz}"
+		GCLOUD_PARENT_PATH="${GCLOUD_PARENT_PATH:-${HOME}/gcloud}"
+		GCLOUD_PATH="${GCLOUD_PATH:-${GCLOUD_PARENT_PATH}/google-cloud-sdk}"
+		wget -P "${GCLOUD_PARENT_PATH}/" \
+                "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${GCLOUD_ARCHIVE}"
+		pushd "${GCLOUD_PARENT_PATH}/" || exit
+		tar xvf "${GCLOUD_ARCHIVE}"
+		rm -vf -- "${GCLOUD_ARCHIVE}"
+		echo "Running the installer"
+		"${GCLOUD_PATH}/install.sh"
+		popd || exit
+
+}
 
 
