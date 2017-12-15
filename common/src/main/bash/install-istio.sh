3ee479a4ee7d6551b7cd9ffdd1c1fad9784bccf7
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-12-15 12:23:55 +0530

    commit

---
+++ common/src/main/bash/install-istio.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+# shellcheck source=/dev/null
+[[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" ||  \
+ echo "No pipeline.sh found"
+
+
+#installing istio 
+downloadIstio
+cd istio-0.3.0
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value core/account)
+kubectl apply -f install/kubernetes/istio.yaml
+#verify istio installation
+kubectl get svc -n istio-system
+
+#installing helm
+
+downloadHelm "false"
+
+helm init
+
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
