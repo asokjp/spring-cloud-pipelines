2e1d3cfef2f2d172cdd07e6be7c19408c71c0e4e
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-12-19 12:54:40 +0530

    commit

--- common/src/main/bash/create_gce_cluster.sh
+++ common/src/main/bash/create_gce_cluster.sh
@@ -20,12 +20,12 @@ GCLOUD_PARENT_PATH="${GCLOUD_PARENT_PATH:-${HOME}/gcloud}"
 			echo "installing gcloud.."
 			downloadGCloud
 		fi
-#echo "moving to base folder"
-#cd /
-#echo "moving to root folder"
-#cd /root
-#echo "listing folder"
-#ls		
+echo "moving to base folder"
+cd /
+echo "moving to root folder"
+cd /root
+echo "listing folder"
+ls		
 source ~/.bashrc
 gcloud container clusters create test --zone us-central1-a --num-nodes 1 --machine-type n1-standard-4
 
