a81b6299140c49f6ff0a50c76b0bc13765b06b1b
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-12-20 11:30:46 +0530

    commit

--- jenkins/jobs/jenkins_pipeline_sample.groovy
+++ jenkins/jobs/jenkins_pipeline_sample.groovy
@@ -48,11 +48,11 @@ String prodDeployrepo="https://github.com/asokjp/prod-env-deploy.git"
 String repos = binding.variables["REPOS"] ?:
 		["https://github.com/asokjp/claimant-service",
 		 "https://github.com/asokjp/hello-world","https://github.com/asokjp/prod-env-deploy"].join(",")
-String fullGitRepoForInfra="https://github.com/asokjp/config-server1"
+String fullGitRepoForInfra="https://github.com/asokjp/prod-env-deploy"
 String branchNameForInfra="master"
-String fullGitRepoForConfigserver="https://github.com/asokjp/prod-env-deploy"
+String fullGitRepoForConfigserver="https://github.com/asokjp/config-server1"
 String branchNameForConfigserver="master"
-String projectNameForConfig = "configserver-pipeline"
+String projectNameForConfig = "configserver1-pipeline"
 	dsl.job("install-kubernetes-cluster") {
 		deliveryPipelineConfiguration('Infra', 'install kubernetes cluster')
 		wrappers {
