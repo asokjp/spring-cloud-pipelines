e344416f21f07cb63a0d90bead385c91df4b390f
Asok Payyapilly <asok.payyapilly@ust-global.com>
2018-01-09 16:49:22 +0530

    commit

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -626,8 +626,8 @@ function applicationHost() {
 
 function ipAddressFromIngress()
 {
-	#local name="${1}"
-	#echo "name is ${name}"
+	local name="${1}"
+	echo "name is ${name}"
 	local ingressName="config-server-gateway"
 	echo "ingressname is"
 	"${KUBECTL_BIN}" --context="${K8S_CONTEXT}" --namespace="${PAAS_NAMESPACE}" get ingress "${ingressName}" -o=yaml | grep -i ip: | awk '{print $3}'
