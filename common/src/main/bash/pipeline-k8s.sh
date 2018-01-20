8a786cb5718bfd2dfd1c460e9044d5a0bea815cf
Asok Payyapilly <asok.payyapilly@ust-global.com>
2018-01-20 23:47:57 +0530

    commit

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -776,8 +776,8 @@ function performGreenDeploymentOfOtherServices {
 		#echo "deployment version of ${projectName} is ${releaseVersion}"
 		local serviceDeployed
 		serviceDeployed="$(objectDeployed "service" "${projectName}")"
-		echo "Service already deployed? [${serviceDeployed}]"
-		if [[ "${serviceDeployed}" == false ]]; then
+		echo "Service already deployed? ${serviceDeployed}"
+		if [[ "${serviceDeployed}" != "false" ]]; then
 			git config --global user.email "asok_jp@yahoo.com"
 			git config --global user.name "asokjp"
 			git clone https://asokjp:Lalithamma1@github.com/asokjp/"${projectName}"  --branch dev/"${version}"
@@ -799,7 +799,7 @@ function performGreenDeploymentOfOtherServices {
 	local ingressFile="gatewayingress.yml"
 	ingressDeployed="$(objectDeployed "ingress" "gateway" )"
 		echo "ingress gateway already deployed? ${ingressDeployed}"
-		if [[ "${ingressDeployed}" == false ]]; then
+		if [[ "${ingressDeployed}" != "false" ]]; then
 			echo "deploying ingress in production"
 			deployApp "${ingressFile}"
 		fi
